6e6c20390f0e32623f52eab4d400c2037bb1dce4
Χαράλαμπος Αναστασίου <39501205+VARANE2003@users.noreply.github.com>
2023-03-29 21:41:30 +0300

    Create sort.h

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT
+#define SORT
+
+#include <vector>
+#include <string>
+#include "csvExtractor.h"
+using namespace std;
+
+void merge(vector<Record> &data, int left, int middle, int right);
+void mergeSort(vector<Record> &data, int left, int right);
+#endif
+Footer
+© 2023 GitHub, Inc.
